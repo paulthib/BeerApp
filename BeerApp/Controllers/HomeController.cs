1a38415912d7f73fcd6900dc80291fc885659096
paulthib <ptweb00@yahoo.com>
2016-07-20 18:09:21 -0600

    angular

--- BeerApp/Controllers/HomeController.cs
+++ BeerApp/Controllers/HomeController.cs
@@ -3,16 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Beer.Data;
+using Beer.Model;
 
 namespace BeerApp.Controllers
 {
     public class HomeController : Controller
     {
+        private IGenericRepository<Ingredient> _repo;
+        public HomeController( )
+        {
+            _repo = new IngredientRepository();
+            _repo.Insert(new Ingredient() {Id = 1, Name = "Hops"});
+            _repo.Insert(new Ingredient() { Id = 2, Name = "Barley" });
+            _repo.Insert(new Ingredient() { Id = 3, Name = "Wheat" });
+        }
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
 
             return View();
+        }
+
+        [HttpPost]
+        public JsonResult GetIngredients()
+        {
+            var data = _repo.SelectAll();
+            var ret = data.Select(x => new { x.Id, x.Name }).ToList();
+            return Json(ret);
         }
     }
 }
---
+++ BeerApp/MyScripts/Ingredients.js
@@ -0,0 +1,21 @@
+﻿
+var app = angular.module('myModule', []);
+
+app.controller('myController', function ($scope, $http) {
+
+    console.log("start controller");
+    getIngredients();
+
+
+    function getIngredients() {
+        $http({
+            method: 'Post',
+            url: '/Home/GetIngredients'
+        }).success(function (data, status, headers, config) {
+            $scope.ingredients = data;
+        }).error(function (data, status, headers, config) {
+            $scope.message = 'Unexpected Error';
+        });
+    }
+
+});
